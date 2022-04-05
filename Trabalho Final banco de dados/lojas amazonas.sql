dbf6e5b7b71429eecc06decd48abba35d58d51de
André <andrellucas093@gmail.com>
2022-04-05 11:33:50 -0300

    Alteracoes em quantidade de inserts

--- Trabalho Final banco de dados/lojas amazonas.sql
+++ Trabalho Final banco de dados/lojas amazonas.sql
@@ -1,25 +1,25 @@
-CREATE TABLE "funcionario" (
+CREATE TABLE if not exists "funcionario" (
   "id" serial Not Null,
   "nome" varchar(50) Not Null,
-  "cpf" varchar(11) Not Null,
-  "salario" decimal(10,2) Not Null,
+  "cpf" varchar(11) not null,
+  "salario" decimal(10,2),
   PRIMARY KEY ("id")
 );
 
-CREATE TABLE "categoria" (
+CREATE TABLE if not exists "categoria" (
   "id" serial Not Null,
   "nome" varchar(20) Not Null,
   PRIMARY KEY ("id")
 );
 
-CREATE TABLE "produto" (
+CREATE TABLE if not exists "produto" (
   "id" serial Not Null,
   "id_funcionario" integer Not Null,
   "id_categoria" integer Not Null,
   "nome_produto" varchar(50) Not Null,
   "qtd_estoque" bigint Not Null,
   "valor_unitario" decimal(10,2) Not Null,
-  "data_cadastro" date,
+  "data_cadastro" date not null default current_date,
   "data_fabricacao" date,
   PRIMARY KEY ("id"),
   CONSTRAINT "FK_produto.id_funcionario"
@@ -30,7 +30,7 @@ CREATE TABLE "produto" (
       REFERENCES "categoria"("id")
 );
 
-CREATE TABLE "cliente" (
+CREATE table if not exists "cliente" (
   "id" serial Not Null,
   "nome" varchar(50) Not Null,
   "login" varchar(50) Not Null,
@@ -41,7 +41,7 @@ CREATE TABLE "cliente" (
   PRIMARY KEY ("id")
 );
 
-CREATE TABLE "endereco" (
+CREATE TABLE if not exists"endereco" (
   "id_cliente" integer not null,
   "uf" char(2) Not Null,
   "cidade" varchar(50) Not Null,
@@ -50,26 +50,26 @@ CREATE TABLE "endereco" (
       REFERENCES "cliente"("id")
 );
 
-CREATE TABLE "pedido" (
+CREATE table if not exists "pedido" (
   "id" serial Not Null,
   "codigo_cliente" integer Not Null,
-  "data_pedido" date Not Null,
+  "data_pedido" date not null default current_date ,
   PRIMARY KEY ("id"),
   CONSTRAINT "FK_pedido.codigo_cliente"
     FOREIGN KEY ("codigo_cliente")
       REFERENCES "cliente"("id")
 );
 
-CREATE TABLE "telefone" (
+CREATE TABLE if not exists"telefone" (
   "id_cliente" integer not null,
   "ddd" varchar(3) Not Null,
-  "num_telefone" varchar,
+  "num_telefone" varchar(9) not null,
   CONSTRAINT "FK_telefone.id_cliente"
     FOREIGN KEY ("id_cliente")
       REFERENCES "cliente"("id")
 );
 
-CREATE TABLE "itens_pedido" (
+CREATE TABLE if not exists "itens_pedido" (
   "id_pedido" integer Not Null,
   "id_produto" integer Not Null,
   "quantidade" integer Not Null,
@@ -107,9 +107,11 @@ insert into cliente(nome, login, senha, email, cpf, data_nascimento) values
 	,('Zoroastro Santana', 'zonzon', '7894', 'zoroastro@gmail.com', '65498732145', '1992/06/22')
 	,('Zatana Dornelas', 'zaza', '45678912356', 'zaza@bol.com.br', '32145698785', '1995/12/08')
 	,('Sidney Magal da Costa', 'cigana20', '6548', 'sandrarosamadalena@outlook.com', '36925814765', '1968/04/03')
+	,('Rilson André Magalhães','rilsim22', '4002', 'rilsimmaga@yahoo.com', '82738893872', '1974/27/07')
 	;
 
-select * from cliente
+
+select * from cliente;
 
 insert into telefone(id_cliente, ddd, num_telefone) values
 	('1','22','988564857')
@@ -117,7 +119,10 @@ insert into telefone(id_cliente, ddd, num_telefone) values
 	,('3','21','992568754')
 	,('4','64','940028922')
 	,('5','22','981459865')
+	,('6','21','994349405')
 	;
+
+
 
 select * from telefone;
 
@@ -130,6 +135,7 @@ insert into endereco(id_cliente, uf, cidade, cep) values
 	,('3','RJ','Teresópolis','25975783')
 	,('4','GO','Água Limpa','75665000')
 	,('5','RJ','Nova Friburgo','28600000')
+	,('6','ES','Guaçuí','29560000')
 	;
 	
 select * from endereco;
@@ -148,16 +154,16 @@ update categoria
 	
 select * from categoria;
 
-insert into produto(id_funcionario, id_categoria, nome_produto, qtd_estoque, valor_unitario, data_cadastro, data_fabricacao) values
-	('1', '1', 'kit kat', 5, 4.99, '2022/04/04', '2022/04/03')
-	,('2', '1', 'M&Ms', 5, 8.99, '2022/04/04', '2022/04/03')
-	,('3', '1', 'kinder ovo', 5, 12.99, '2022/04/04', '2022/04/03')
-	,('4', '1', 'amandita', 5, 11.99, '2022/04/04', '2022/04/03')
-	,('5', '1', 'tortuguita', 5, 9.99, '2022/04/04', '2022/04/03')
-	,('6', '1', 'maria mole', 5, 0.25, '2022/04/04', '2022/04/03')
+insert into produto(id_funcionario, id_categoria, nome_produto, qtd_estoque, valor_unitario, data_fabricacao) values
+	('1', '1', 'kit kat', 5, 4.99, '2022/04/03')
+	,('2', '1', 'M&Ms', 5, 8.99, '2022/04/03')
+	,('3', '1', 'kinder ovo', 5, 12.99, '2022/04/03')
+	,('4', '1', 'amandita', 5, 11.99, '2022/04/03')
+	,('5', '1', 'tortuguita', 5, 9.99, '2022/04/03')
+	,('6', '1', 'maria mole', 5, 0.25, '2022/04/03')
 ;
+select 
 
-select * from produto;
-
+insert into pedido ()
 --alter table endereco
 	--rename column mariola to cep;
