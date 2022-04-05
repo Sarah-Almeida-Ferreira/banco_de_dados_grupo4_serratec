655511eac65edaa45e5c0daa26b2c7f4f64c4f9e
chevrand <wandersonchevrand27@gmail.com>
2022-04-04 22:42:38 -0300

    insert de dados

--- Trabalho Final banco de dados/lojas amazonas.sql
+++ Trabalho Final banco de dados/lojas amazonas.sql
@@ -101,3 +101,63 @@ insert into funcionario(nome, cpf, salario) values
 
 select id, nome, cpf, salario from funcionario;
 
+insert into cliente(nome, login, senha, email, cpf, data_nascimento) values
+	('Amazonilson Pereira da Silva', 'amazon', '1234', 'amazonilson@hotmail.com', '12345678985', '1980/05/12')
+	,('Jhonatans Bittencourt', 'joninha', '4321', 'joninhasrd@hotmail.com', '98765432125', '1990/05/12')
+	,('Zoroastro Santana', 'zonzon', '7894', 'zoroastro@gmail.com', '65498732145', '1992/06/22')
+	,('Zatana Dornelas', 'zaza', '45678912356', 'zaza@bol.com.br', '32145698785', '1995/12/08')
+	,('Sidney Magal da Costa', 'cigana20', '6548', 'sandrarosamadalena@outlook.com', '36925814765', '1968/04/03')
+	;
+
+select * from cliente
+
+insert into telefone(id_cliente, ddd, num_telefone) values
+	('1','22','988564857')
+	,('2','21','988652354')
+	,('3','21','992568754')
+	,('4','64','940028922')
+	,('5','22','981459865')
+	;
+
+select * from telefone;
+
+alter table endereco
+	add cep varchar(8);
+
+insert into endereco(id_cliente, uf, cidade, cep) values
+	('1','RJ','Nova Friburgo','28600000')
+	,('2','RJ','Teresópolis','25956170')
+	,('3','RJ','Teresópolis','25975783')
+	,('4','GO','Água Limpa','75665000')
+	,('5','RJ','Nova Friburgo','28600000')
+	;
+	
+select * from endereco;
+
+update categoria
+	set nome = 'doces'
+		where id = '1';
+	
+update categoria
+	set nome ='eletrônicos'
+		where id = '2';
+	
+update categoria
+	set nome ='vestuário'
+		where id = '3';
+	
+select * from categoria;
+
+insert into produto(id_funcionario, id_categoria, nome_produto, qtd_estoque, valor_unitario, data_cadastro, data_fabricacao) values
+	('1', '1', 'kit kat', 5, 4.99, '2022/04/04', '2022/04/03')
+	,('2', '1', 'M&Ms', 5, 8.99, '2022/04/04', '2022/04/03')
+	,('3', '1', 'kinder ovo', 5, 12.99, '2022/04/04', '2022/04/03')
+	,('4', '1', 'amandita', 5, 11.99, '2022/04/04', '2022/04/03')
+	,('5', '1', 'tortuguita', 5, 9.99, '2022/04/04', '2022/04/03')
+	,('6', '1', 'maria mole', 5, 0.25, '2022/04/04', '2022/04/03')
+;
+
+select * from produto;
+
+--alter table endereco
+	--rename column mariola to cep;
